e76bd54a7f683975fdb0884e076f1641b83c8ed4
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-27 14:40:29 +0300

    Create index routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ const App = function () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
           <Route path="cities" element={<p>List of cities</p>} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
