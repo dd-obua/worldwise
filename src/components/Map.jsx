8d051b6a8c0934928f542f225ec8f6937d48c680
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-27 19:22:37 +0300

    Read lat and lng accessible globally from Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,22 @@
+import { useSearchParams } from 'react-router-dom';
+
 import styles from './Map.module.css';
 
 const Map = function () {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  setSearchParams;
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat} {lng}
+      </h2>
+    </div>
+  );
 };
 
 export default Map;
