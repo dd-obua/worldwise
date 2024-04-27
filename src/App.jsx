db20e9df22898a525858f23534716417860b0943
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-27 15:57:59 +0300

    Fetch cities data

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,31 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:8000';
 
 const App = function () {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${baseUrl}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('There was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
