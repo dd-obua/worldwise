db5f23ca6d9ece51dcecd3bcde31fc14bc0cd85f
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-27 19:38:39 +0300

    Change url information globally using useSearchParams

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,14 +7,19 @@ const Map = function () {
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
-  setSearchParams;
-
   return (
     <div className={styles.mapContainer}>
       <h1>Map</h1>
-      <h2>
+      <p>
         Position: {lat} {lng}
-      </h2>
+      </p>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change position
+      </button>
     </div>
   );
 };
