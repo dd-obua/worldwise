cf9eb5518544ef17b9381bb073d9d996d44aadc1
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-27 14:56:44 +0300

    Create framework for city list

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from './pages/Pricing';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import CityList from './components/CityList';
 
 const App = function () {
   return (
@@ -17,7 +18,7 @@ const App = function () {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from './CityList.module.css';
+
+const CityList = function () {
+  return (
+    <ul className={styles.cityList}>
+      <li>City</li>
+    </ul>
+  );
+};
+
+export default CityList;
