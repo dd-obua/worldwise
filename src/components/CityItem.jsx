b2cdef1af47e2b1d389d84703a8d066d96dab4cd
Obua Denis Daniel <dd.obua@gmail.com>
2024-05-03 20:04:32 +0300

    Highlight current city

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
-import styles from './CityItem.module.css';
+
 import { Link } from 'react-router-dom';
+
+import styles from './CityItem.module.css';
+import { useCities } from '../contexts/CitiesContexts';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat('en', {
@@ -11,12 +14,15 @@ const formatDate = (date) =>
 
 const CityItem = function ({ city }) {
   const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
 
   return (
     <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          currentCity.id === id && styles['cityItem--active']
+        }`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
