36dcc26ca854259a3f339412096adf5d9f3ef1eb
Obua Denis Daniel <dd.obua>
2024-05-07 17:07:53 +0300

    Change version number on leaflet.css

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,16 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from "react-router-dom";
 
-import styles from './Map.module.css';
+import styles from "./Map.module.css";
 
 const Map = function () {
   const navigate = useNavigate();
 
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <p>
-        Position: {lat} {lng}
-      </p>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
-      >
-        Change position
-      </button>
-    </div>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}></div>
   );
 };
 
