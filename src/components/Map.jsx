d49b98cfcba3fa93672c3024258f7884eb1b481c
Obua Denis Daniel <dd.obua>
2024-05-08 11:14:44 +0300

    Move map to users own position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-import { useNavigate, useSearchParams } from "react-router-dom";
-
 import styles from "./Map.module.css";
+
+import { useNavigate, useSearchParams } from "react-router-dom";
 import {
   MapContainer,
   Marker,
@@ -13,12 +13,19 @@ import {
 } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContexts";
+import { useGeolocation } from "../hooks/useGeolocation";
+
+import Button from "./Button";
 
 const Map = function () {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
@@ -30,8 +37,22 @@ const Map = function () {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position."}
+        </Button>
+      )}
+
       <MapContainer
         center={mapPosition}
         zoom={6}
