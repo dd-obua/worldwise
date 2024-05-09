c8cc6f811e4cb508a1066d15ec65c2893c03e897
Obua Denis Daniel <dd.obua>
2024-05-09 12:42:26 +0300

    Logout and return to home page

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,15 @@
-import Sidebar from '../components/Sidebar';
-import Map from '../components/Map';
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 
-import styles from './AppLayout.module.css';
+import styles from "./AppLayout.module.css";
+import User from "../components/User";
 
 const AppLayout = function () {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 };
